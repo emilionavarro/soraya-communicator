8e298e330847b4a4e53b7a9bf9b0ea32dcd4d65f
Emilio Navarro Cruz <emilio.mst@gmail.com>
2017-11-06 10:11:21 -0600

    Adding in artificial slowdown for heroku

--- index.js
+++ index.js
@@ -26,7 +26,10 @@ app.get('/', function(request, response) {
 app.post('/sms', (req, res) => {
   // const twiml = new MessagingResponse();
   //twiml.message('The Robots are coming! Head for the hills!');
-  handleNewMessage();
+  setTimeout(function(){ 
+    handleNewMessage(); 
+  }, 1000);
+
   res.end();
   //res.writeHead(200, {'Content-Type': 'text/xml'});
   //res.end(twiml.toString());
